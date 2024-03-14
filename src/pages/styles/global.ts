5d6766eca7aa76e3a8a0ad43de2e62b26c1effa8
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-03-14 14:24:55 -0300

    ESlint setup

--- src/pages/styles/global.ts
+++ src/pages/styles/global.ts
@@ -1,16 +1,16 @@
-import { globalCss } from "@ignite-ui/react";
+import { globalCss } from '@ignite-ui/react'
 
 export const globalStyles = globalCss({
   '*': {
     boxSizing: 'border-box',
     margin: 0,
     padding: 0,
-    fontFamily: 'Roboto, sans-serif'
+    fontFamily: 'Roboto, sans-serif',
   },
-  
+
   body: {
     backgroundColor: '$gray900',
     color: '$gray100',
-    '-webkit-font-smoothing': 'antialiased'
-  }
+    '-webkit-font-smoothing': 'antialiased',
+  },
 })
