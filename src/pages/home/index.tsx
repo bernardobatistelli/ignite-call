2c6dea9a15f5788ae7370a0a0977d2af837ebf97
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-03-25 13:21:13 -0300

    Seo optimizations

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,29 +2,37 @@ import { Heading, Text } from '@ignite-ui/react'
 import { Container, Hero, Preview } from './styles'
 import Image from 'next/image'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos
+        no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
 
-        <ClaimUsernameForm />
-      </Hero>
-      <Preview>
-        <Image
-          src="/image.png"
-          alt="Calendário simbolizando aplicação em funcionamento"
-          width={850}
-          height={450}
-          quality={100}
-          priority
-        />
-      </Preview>
-    </Container>
+          <ClaimUsernameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src="/image.png"
+            alt="Calendário simbolizando aplicação em funcionamento"
+            width={850}
+            height={450}
+            quality={100}
+            priority
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
